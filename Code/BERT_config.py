1fe91fd9b9c6278251496c3575e2c8176e70cd94
Manas Ranjan Mohanty <42920503+ManasRMohanty@users.noreply.github.com>
2020-12-16 18:54:56 -0500

    Made final changes
    Made final changes

--- Code/BERT_config.py
+++ Code/BERT_config.py
@@ -1,2 +1,3 @@
 bert_config = {'entity_finetuned_model_path':"C:/Users/itsma/Documents/CS 6120 Project/CS6120/Model/finetuned_model.pkl",
+               'relation_finetuned_model_path':"C:/Users/itsma/Documents/CS 6120 Project/CS6120/Model/relation_finetuned.pkl",
                'ncbi_base_path':"C:/Users/itsma/Documents/BERT_models/NCBI_BERT_pubmed_mimic_uncased_L-12_H-768_A-12"}
